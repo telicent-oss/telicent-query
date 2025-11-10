8c4ea807c676a33e6eb0e55e97e75ad8ffc250f1
Dave Cockrell <dave@telicent.io>
2025-11-10 10:38:44 +0000

    chore(package.json): update dependencies

--- override.env-config.js
+++ override.env-config.js
@@ -1,6 +1,6 @@
 window.GRAPHQL_URL = 'http://localhost:3000/graph/knowledge';
 
-window.SPARQL_URL = 'http://localhost:3000/g/knowledge/sparql';
+window.SPARQL_URL = 'http://localhost:3000/graph/knowledge/sparql';
 
 window.ACCESS_URL = 'http://localhost:8091';
 // For MAP_TILER_TOKEN see ./sensitive/secret-config.js
