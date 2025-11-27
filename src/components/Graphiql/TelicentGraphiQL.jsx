e6b1403e9ecd2ced10d92a90c7deeff195d5a5aa
Dave Cockrell <dave@telicent.io>
2025-11-27 22:07:01 +0000

    fix: styles

--- src/components/Graphiql/TelicentGraphiQL.jsx
+++ src/components/Graphiql/TelicentGraphiQL.jsx
@@ -1,6 +1,7 @@
 import { createGraphiQLFetcher } from '@graphiql/toolkit';
 import { TeliBrand, useAuth } from '@telicent-oss/ds';
 import { GraphiQL } from 'graphiql';
+import 'graphiql/graphiql.css';
 import config from '../../config/app-config';
 import { useCallback } from 'react';
 
@@ -17,7 +18,15 @@ const TelicentGraphiQLNew = () => {
     [api, user],
   );
 
-  return <GraphiQL fetcher={fetcher} />;
+  return (
+    <section style={{ height: '100vh' }}>
+      <GraphiQL fetcher={fetcher}>
+        <GraphiQL.Logo>
+          <TeliBrand appName="queryQL" className="flex items-center justify-center" />
+        </GraphiQL.Logo>
+      </GraphiQL>
+    </section>
+  );
 };
 
 const TelicentGraphiQLLegacy = () => {
